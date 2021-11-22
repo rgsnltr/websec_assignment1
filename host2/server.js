0a9880bb0eb23b012692175411b35f819b260b4d
rgsnltr <79885574+rgsnltr@users.noreply.github.com>
2021-11-22 16:57:55 +0800

    Add files via upload

---
+++ host2/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { createReadStream } = require('fs') 
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.listen(80)
+app.get('/', (req, res) => {
+	createReadStream('index.html').pipe(res)
+})
+app.get('/api/date', (req, res) => {
+	res.send({ date: Date.now() })
+})
+app.get('/api/datecors', (req, res) => {
+	res.set('Access-Control-Allow-Origin', '*')
+	res.send({ date: Date.now() })
+})
+app.get('/api/jsonpdate', (req, res) => {
+	res.send(req.query.callback + '({ "date": ' + Date.now() + ' })')
+})
